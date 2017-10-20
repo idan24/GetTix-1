ceb9b7ea8bbbc3315028db146b4375a909104026
almogrubi <almog.rubinstein@gmail.com>
2017-10-20 04:28:06 +0300

    added payactivity layout and started working on email sending.

--- app/src/main/java/app/com/almogrubi/idansasson/gettix/PayActivity.java
+++ app/src/main/java/app/com/almogrubi/idansasson/gettix/PayActivity.java
@@ -1,14 +1,19 @@
 package app.com.almogrubi.idansasson.gettix;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
- * Created by almogrubi on 10/17/17.
+ * Created by almogrubi on 10/20/17.
  */
 
 public class PayActivity extends AppCompatActivity {
@@ -19,6 +24,35 @@ public class PayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pay);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button next = (Button) findViewById(R.id.button_pick_sits);
+
+
+/*        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(v.getContext(), SitsActivity.class);
+                EditText emailInput = (EditText) findViewById(R.id.email_edit_text);
+                String email = emailInput.getText().toString();
+
+                //sending email code
+                Intent intent = new Intent(Intent.ACTION_SENDTO); // it's not ACTION_SEND
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Subject of email");
+                intent.putExtra(Intent.EXTRA_TEXT, "Body of email");
+                intent.setData(Uri.parse("mailto:default@recipient.com")); // or just "mailto:" for blank
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
+                startActivity(intent);
+
+
+            }
+        });*/
+
+
+
+
+
+
 
     }
 
